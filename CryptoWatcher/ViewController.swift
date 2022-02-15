de6bd4aca24fd115d1b0c6e51f52e088a5aa9f38
meschya <meschyaaa@gmail.com>
2022-02-16 00:58:24 +0300

    Initial Commit

---
+++ CryptoWatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWatcher
+//
+//  Created by Egor Mesheryakov on 16.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
